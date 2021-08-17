a61cf44b4b21223ed0130531b2bca3361da2d4d2
Matthew Juan <matthew.juan23@gmail.com>
2021-08-17 09:40:51 +0000

    first production release

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ TOKEN = os.environ['DISCORD_TOKEN']
 
 client = discord.Client()
 
+# random quote
+
+# random quote from day
+
 @client.event
 async def on_ready():
     print(f"{client.user.name} connected to Discord")
@@ -16,7 +20,13 @@ async def on_message(message):
         # don't want to infinitely respond to self
         return
 
-    print(message.author)
+    if str(message.author) == "odell#7491":
+        await message.channel.send("Shut up Odell")
 
+    if str(message.author) == "zachyboiii#5666":
+        await message.channel.send("Shut up Zach")
+    
+    if str(message.author) == "BKEmbers#0351":
+        await message.channel.send("Ben Kwok: \"truth is I like semen in my ass\" - Monday 14 November")
 
 client.run(TOKEN)
